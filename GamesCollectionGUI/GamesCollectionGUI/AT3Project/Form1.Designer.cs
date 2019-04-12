cae965361c33bcd07057a33c43bc15dfb50d6dfc
LukeGough <37359964+LukeGough@users.noreply.github.com>
2019-04-12 09:45:12 +0800

    Add files via upload

---
+++ GamesCollectionGUI/GamesCollectionGUI/AT3Project/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace AT3Project
+{
+    partial class ComputerGamesCollectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbPlatform = new System.Windows.Forms.TextBox();
+            this.tbGenre = new System.Windows.Forms.TextBox();
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbPlatform = new System.Windows.Forms.Label();
+            this.lbGenre = new System.Windows.Forms.Label();
+            this.tbArrayIndex = new System.Windows.Forms.TextBox();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(12, 12);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(314, 433);
+            this.lstOutput.TabIndex = 0;
+            this.lstOutput.SelectedIndexChanged += new System.EventHandler(this.lstOutput_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(388, 102);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(469, 102);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(388, 131);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(469, 131);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(388, 160);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(469, 160);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(388, 218);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(469, 218);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 8;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(433, 9);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(111, 20);
+            this.tbName.TabIndex = 9;
+            // 
+            // tbPlatform
+            // 
+            this.tbPlatform.Location = new System.Drawing.Point(433, 35);
+            this.tbPlatform.Name = "tbPlatform";
+            this.tbPlatform.Size = new System.Drawing.Size(111, 20);
+            this.tbPlatform.TabIndex = 10;
+            // 
+            // tbGenre
+            // 
+            this.tbGenre.Location = new System.Drawing.Point(433, 61);
+            this.tbGenre.Name = "tbGenre";
+            this.tbGenre.Size = new System.Drawing.Size(111, 20);
+            this.tbGenre.TabIndex = 11;
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(385, 12);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(38, 13);
+            this.lbName.TabIndex = 12;
+            this.lbName.Text = "Name:";
+            // 
+            // lbPlatform
+            // 
+            this.lbPlatform.AutoSize = true;
+            this.lbPlatform.Location = new System.Drawing.Point(385, 38);
+            this.lbPlatform.Name = "lbPlatform";
+            this.lbPlatform.Size = new System.Drawing.Size(48, 13);
+            this.lbPlatform.TabIndex = 13;
+            this.lbPlatform.Text = "Platform:";
+            // 
+            // lbGenre
+            // 
+            this.lbGenre.AutoSize = true;
+            this.lbGenre.Location = new System.Drawing.Point(385, 64);
+            this.lbGenre.Name = "lbGenre";
+            this.lbGenre.Size = new System.Drawing.Size(39, 13);
+            this.lbGenre.TabIndex = 14;
+            this.lbGenre.Text = "Genre:";
+            // 
+            // tbArrayIndex
+            // 
+            this.tbArrayIndex.Location = new System.Drawing.Point(469, 418);
+            this.tbArrayIndex.Name = "tbArrayIndex";
+            this.tbArrayIndex.ReadOnly = true;
+            this.tbArrayIndex.Size = new System.Drawing.Size(75, 20);
+            this.tbArrayIndex.TabIndex = 15;
+            // 
+            // ComputerGamesCollectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 450);
+            this.Controls.Add(this.tbArrayIndex);
+            this.Controls.Add(this.lbGenre);
+            this.Controls.Add(this.lbPlatform);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.tbGenre);
+            this.Controls.Add(this.tbPlatform);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstOutput);
+            this.Name = "ComputerGamesCollectionForm";
+            this.Text = "Computer Games Collection";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ComputerGamesCollectionForm_FormClosing);
+            this.Load += new System.EventHandler(this.ComputerGamesCollectionForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbPlatform;
+        private System.Windows.Forms.TextBox tbGenre;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbPlatform;
+        private System.Windows.Forms.Label lbGenre;
+        private System.Windows.Forms.TextBox tbArrayIndex;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ GamesCollectionGUI/GamesCollectionGUI/AT3Project/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AT3Project
+{
+    public partial class ComputerGamesCollectionForm : Form
+    {
+        private string[] names;
+        private string[] platforms;
+        private string[] genres;
+        int numItems;
+
+        public ComputerGamesCollectionForm()
+        {
+            InitializeComponent();
+            names = new string[20];
+            platforms = new string[20];
+            genres = new string[20];
+            numItems = 0;
+
+            // display current amount of items in array into tbArrayIndex
+            tbArrayIndex.Text = ((numItems).ToString());
+            
+            // load information from mygames.data
+            Open();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (tbName.Text == "")  // check if text boxe are empty
+            {
+                MessageBox.Show("Please enter a Name");
+                return;
+            }
+            else if (tbPlatform.Text == "") // check if text boxe are empty
+            {
+                MessageBox.Show("Please enter a Platform");
+                return;
+            }
+            else if (tbGenre.Text == "")    // check if text boxe are empty
+            {
+                MessageBox.Show("Please enter a Genre");
+                return;
+            }
+            else
+            {
+                names[numItems] = tbName.Text;
+                platforms[numItems] = tbPlatform.Text;
+                genres[numItems] = tbGenre.Text;
+                numItems++;
+                Clear();
+                DisplayGames();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int index = lstOutput.SelectedIndex;
+            if (index == -1)
+            {
+                MessageBox.Show("Select an item");
+                return;
+            }
+
+            DeleteAt(names, platforms, genres, index);
+            Clear();
+            DisplayGames();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+            DisplayGames();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int index = lstOutput.SelectedIndex;
+            if (index == -1)
+            {
+                MessageBox.Show("Select an item");
+                return;
+            }
+            else
+            {
+                names[index] = tbName.Text;
+                platforms[index] = tbPlatform.Text;
+                genres[index] = tbGenre.Text;
+
+                lstOutput.Items.RemoveAt(index);
+                //String updated = (tbName.Text + " " + tbPlatform.Text + " " +tbGenre.Text);
+                //lstOutput.Items.Insert(index, updated);
+                //lstOutput.Items.Insert(index, (tbName.Text + " " + tbPlatform.Text + " " + tbGenre.Text));
+                lstOutput.Items.Insert(index, (names[index] + " " + platforms[index] + " " + genres[index]));
+            }
+
+            Clear();
+            DisplayGames();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            Open();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string search = tbName.Text;
+            int index = BinarySearch(names, search);
+
+            if (index != -1)
+            {
+                tbPlatform.Text = platforms[index];
+                tbGenre.Text = genres[index];
+
+                // focus on the searched item
+                lstOutput.SelectedIndex = index;
+                lstOutput.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Game not found");
+                tbName.Clear();
+                tbPlatform.Clear();
+                tbGenre.Clear();
+            }
+        }
+
+        private void Save()
+        {
+            try
+            {
+                // Saves to \AT3Project\AT3Project\bin\Debug\mygames.dat
+                using (BinaryWriter writer = new BinaryWriter(File.Open("mygames.dat", FileMode.Create)))
+                {
+                    writer.Write(numItems);
+                    for (int i = 0; i < numItems; i++)
+                    {
+                        //writer.Write(names[i] + " " + platforms[i] + " " + genres[i]);
+                        writer.Write(names[i]);
+                        writer.Write(platforms[i]);
+                        writer.Write(genres[i]);
+                    }
+
+                    MessageBox.Show("File has been saved", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Exception: " + ex);
+            }
+        }
+
+        private void Open()
+        {
+            try
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open("mygames.dat", FileMode.Open)))
+                {
+                    numItems = reader.ReadInt32();
+                    for (int i = 0; i < numItems; i++)
+                    {
+                        string name = reader.ReadString();
+                        string platform = reader.ReadString();
+                        string genre = reader.ReadString();
+
+                        names[i] = name;
+                        platforms[i] = platform;
+                        genres[i] = genre;
+                    }
+                    DisplayGames();
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Exception: " + ex);
+            }
+
+        }
+
+        private void Clear()
+        {
+            // clear all text boxes
+            tbName.Clear();
+            tbPlatform.Clear();
+            tbGenre.Clear();
+        }
+
+        private void Reset()
+        {
+            // the for loop does work but isn't required as numItems is set to 0
+            // because numItems is set to 0 the previous items in the arrays will be overwritten
+            //for (int i = 0; i < numItems; i++)
+            //{
+            //    names[i] = null;
+            //    platforms[i] = null;
+            //    genres[i] = null;
+            //}
+            numItems = 0;
+        }
+
+        private void DeleteAt(string[] names, string[] platforms, string[] genres, int index)
+        {
+            for (int i = index; i < numItems - 1; i++)
+            {
+                // shuffling items in the array, basically moving the item into the previous array index
+                names[i] = names[i + 1];
+                platforms[i] = platforms[i + 1];
+                genres[i] = genres[i + 1];
+            }
+            numItems--;
+        }
+
+        private void DisplayGames()
+        {
+            lstOutput.Items.Clear();
+            BubbleSort(names, platforms, genres);
+            for (int i = 0; i < numItems; i++)
+            {
+                // display arrays in lstOutput
+                lstOutput.Items.Add(names[i] + " " + platforms[i] + " " + genres[i]);
+            }
+            tbArrayIndex.Text = ((numItems).ToString());
+        }
+
+        private void BubbleSort(string[] names, string[] platforms, string[] genres)
+        {
+            for (int outer = 0; outer < numItems; outer++)
+            {
+                for (int inner = 0; inner < (numItems - outer - 1); inner++)
+                {
+                    if (string.Compare(names[inner], names[inner + 1]) > 0)
+                    {
+                        // saving inner + 1 array into a temp string
+                        // replacing inner + 1 with inner
+                        // replacing inner with tmp, which is actually inner + 1
+
+                        string tmp = names[inner + 1];
+                        names[inner + 1] = names[inner];
+                        names[inner] = tmp;
+
+                        tmp = platforms[inner + 1];
+                        platforms[inner + 1] = platforms[inner];
+                        platforms[inner] = tmp;
+
+                        tmp = genres[inner + 1];
+                        genres[inner + 1] = genres[inner];
+                        genres[inner] = tmp;
+                    }
+                }
+            }
+        }
+
+        private void lstOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = lstOutput.SelectedIndex;
+
+            // if statement is need to check if nothing is selected
+            // if nothing is selected, return (contiune with rest of code)
+            if (index == -1)
+            {
+                return;
+            }
+
+            tbName.Text = names[index];
+            tbPlatform.Text = platforms[index];
+            tbGenre.Text = genres[index];
+        }
+
+        private void ComputerGamesCollectionForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // call save method when closing program
+            Save();
+        }
+
+        private void ComputerGamesCollectionForm_Load(object sender, EventArgs e)
+        {
+            // setup tooltips for text boxes on form load
+            toolTip.SetToolTip(tbName, "Enter Game Name");
+            toolTip.SetToolTip(tbPlatform, "Enter Platform Name");
+            toolTip.SetToolTip(tbGenre, "Enter Genre Name");
+        }
+
+        private int BinarySearch(string[] names, string search)
+        {
+            int lowerBound = 0;
+            int upperBound = numItems;
+            int mid;
+
+            while (true)
+            {
+                mid = (lowerBound + upperBound) / 2;
+                if (names[mid] == search)
+                {
+                    return mid;
+                }
+                else if (lowerBound > upperBound)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (string.Compare(names[mid], search, false) > 0)
+                    {
+                        upperBound = mid - 1;
+                    }
+                    else
+                    {
+                        lowerBound = mid + 1;
+                    }
+                }
+            }
+        }
+    }
+}
