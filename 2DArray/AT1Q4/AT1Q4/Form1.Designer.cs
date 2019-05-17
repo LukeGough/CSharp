f575c9a1c0a4c168ad00ec7db6856439d8d737d1
LukeGough <37359964+LukeGough@users.noreply.github.com>
2019-05-17 09:48:28 +0800

    Add files via upload

---
+++ 2DArray/AT1Q4/AT1Q4/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace AT1Q4
+{
+    partial class Question4Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbGameTitle = new System.Windows.Forms.Label();
+            this.lbPlatform = new System.Windows.Forms.Label();
+            this.lbGenre = new System.Windows.Forms.Label();
+            this.lbPublisher = new System.Windows.Forms.Label();
+            this.tbGameTitle = new System.Windows.Forms.TextBox();
+            this.tbPlatform = new System.Windows.Forms.TextBox();
+            this.tbGenre = new System.Windows.Forms.TextBox();
+            this.tbPublisher = new System.Windows.Forms.TextBox();
+            this.lbMyGames = new System.Windows.Forms.Label();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbGameTitle
+            // 
+            this.lbGameTitle.AutoSize = true;
+            this.lbGameTitle.Location = new System.Drawing.Point(46, 13);
+            this.lbGameTitle.Name = "lbGameTitle";
+            this.lbGameTitle.Size = new System.Drawing.Size(58, 13);
+            this.lbGameTitle.TabIndex = 0;
+            this.lbGameTitle.Text = "Game Title";
+            // 
+            // lbPlatform
+            // 
+            this.lbPlatform.AutoSize = true;
+            this.lbPlatform.Location = new System.Drawing.Point(59, 45);
+            this.lbPlatform.Name = "lbPlatform";
+            this.lbPlatform.Size = new System.Drawing.Size(45, 13);
+            this.lbPlatform.TabIndex = 1;
+            this.lbPlatform.Text = "Platform";
+            // 
+            // lbGenre
+            // 
+            this.lbGenre.AutoSize = true;
+            this.lbGenre.Location = new System.Drawing.Point(68, 77);
+            this.lbGenre.Name = "lbGenre";
+            this.lbGenre.Size = new System.Drawing.Size(36, 13);
+            this.lbGenre.TabIndex = 2;
+            this.lbGenre.Text = "Genre";
+            // 
+            // lbPublisher
+            // 
+            this.lbPublisher.AutoSize = true;
+            this.lbPublisher.Location = new System.Drawing.Point(54, 111);
+            this.lbPublisher.Name = "lbPublisher";
+            this.lbPublisher.Size = new System.Drawing.Size(50, 13);
+            this.lbPublisher.TabIndex = 3;
+            this.lbPublisher.Text = "Publisher";
+            // 
+            // tbGameTitle
+            // 
+            this.tbGameTitle.Location = new System.Drawing.Point(110, 10);
+            this.tbGameTitle.Name = "tbGameTitle";
+            this.tbGameTitle.Size = new System.Drawing.Size(100, 20);
+            this.tbGameTitle.TabIndex = 4;
+            // 
+            // tbPlatform
+            // 
+            this.tbPlatform.Location = new System.Drawing.Point(110, 42);
+            this.tbPlatform.Name = "tbPlatform";
+            this.tbPlatform.Size = new System.Drawing.Size(100, 20);
+            this.tbPlatform.TabIndex = 5;
+            // 
+            // tbGenre
+            // 
+            this.tbGenre.Location = new System.Drawing.Point(110, 74);
+            this.tbGenre.Name = "tbGenre";
+            this.tbGenre.Size = new System.Drawing.Size(100, 20);
+            this.tbGenre.TabIndex = 6;
+            // 
+            // tbPublisher
+            // 
+            this.tbPublisher.Location = new System.Drawing.Point(110, 108);
+            this.tbPublisher.Name = "tbPublisher";
+            this.tbPublisher.Size = new System.Drawing.Size(100, 20);
+            this.tbPublisher.TabIndex = 7;
+            // 
+            // lbMyGames
+            // 
+            this.lbMyGames.AutoSize = true;
+            this.lbMyGames.Location = new System.Drawing.Point(9, 137);
+            this.lbMyGames.Name = "lbMyGames";
+            this.lbMyGames.Size = new System.Drawing.Size(57, 13);
+            this.lbMyGames.TabIndex = 8;
+            this.lbMyGames.Text = "My Games";
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(12, 153);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(348, 173);
+            this.lstOutput.TabIndex = 9;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(232, 10);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(232, 34);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(232, 82);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(232, 106);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 13;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(232, 58);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Question4Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 338);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.lbMyGames);
+            this.Controls.Add(this.tbPublisher);
+            this.Controls.Add(this.tbGenre);
+            this.Controls.Add(this.tbPlatform);
+            this.Controls.Add(this.tbGameTitle);
+            this.Controls.Add(this.lbPublisher);
+            this.Controls.Add(this.lbGenre);
+            this.Controls.Add(this.lbPlatform);
+            this.Controls.Add(this.lbGameTitle);
+            this.Name = "Question4Form";
+            this.Text = "Question 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbGameTitle;
+        private System.Windows.Forms.Label lbPlatform;
+        private System.Windows.Forms.Label lbGenre;
+        private System.Windows.Forms.Label lbPublisher;
+        private System.Windows.Forms.TextBox tbGameTitle;
+        private System.Windows.Forms.TextBox tbPlatform;
+        private System.Windows.Forms.TextBox tbGenre;
+        private System.Windows.Forms.TextBox tbPublisher;
+        private System.Windows.Forms.Label lbMyGames;
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ 2DArray/AT1Q4/AT1Q4/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AT1Q4
+{
+    public partial class Question4Form : Form
+    {
+        static int maxRowSize;
+        static int maxColSize;
+        int numItems;
+        private string[,] games;
+
+        public Question4Form()
+        {
+            InitializeComponent();
+            maxRowSize = 10;
+            maxColSize = 4;
+            numItems = 0;
+            games = new string[maxRowSize, maxColSize];
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (numItems == maxRowSize)
+                {
+                    MessageBox.Show("Games collection is full");
+                    return;
+                }
+                else
+                {
+                    // store textbox valus in Row i (numItems), Columns 1, 2, 3 & 4
+                    games[numItems, 0] = tbGameTitle.Text;
+                    games[numItems, 1] = tbPlatform.Text;
+                    games[numItems, 2] = tbGenre.Text;
+                    games[numItems, 3] = tbPublisher.Text;
+
+                    // increase numItem so the next new Game can be created
+                    numItems++;
+
+                    // clear textboxes
+                    Clear();
+
+                    // display games
+                    DisplayGames();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception" + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string search = tbGameTitle.Text;
+            int index = BinarySearch(games, search);
+
+            if (index != -1)
+            {
+                tbPlatform.Text = games[index, 1];
+                tbGenre.Text = games[index, 2];
+                tbPublisher.Text = games[index, 3];
+
+                // focus on the searched item
+                lstOutput.SelectedIndex = index;
+                lstOutput.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Game not found");
+                Clear();
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            Open();
+        }
+
+        // method to open games.dat
+        private void Open()
+        {
+            try
+            {
+                // create new BinaryReader named reader and open file "games.dat"
+                using (BinaryReader reader = new BinaryReader(File.Open("games.dat", FileMode.Open)))
+                {
+                    // read int numItems
+                    numItems = reader.ReadInt32();
+
+                    // loop through and create + read string
+                    // then assign string to array
+                    for (int i = 0; i < numItems; i++)
+                    {
+                        games[i, 0] = reader.ReadString();
+                        games[i, 1] = reader.ReadString();
+                        games[i, 2] = reader.ReadString();
+                        games[i, 3] = reader.ReadString();
+                    }
+
+                    // call DisplayGames method
+                    DisplayGames();
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Exception" + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // method to save games array to games.dat
+        private void Save()
+        {
+            try
+            {
+                // call BubbleSort method with games array
+                BubbleSort(games);
+
+                // Saves to \AT1Q4\AT1Q4\AT1Q4\bin\Debug\games.dat
+                using (BinaryWriter writer = new BinaryWriter(File.Open("games.dat", FileMode.Create)))
+                {
+                    // write current number of items in game array
+                    writer.Write(numItems);
+
+                    // loop through and write the items in the games array to binary file
+                    for (int i = 0; i < numItems; i++)
+                    {
+                        writer.Write(games[i, 0]);
+                        writer.Write(games[i, 1]);
+                        writer.Write(games[i, 2]);
+                        writer.Write(games[i, 3]);
+                    }
+
+                    // show message box telling user file has been saved
+                    MessageBox.Show("File has been saved", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Exception" + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // bubble sort method
+        private void BubbleSort(string[,] games)
+        {
+            for (int outer = 0; outer < numItems; outer++)
+            {
+                for (int inner = 0; inner < (numItems - outer - 1); inner++)
+                {
+                    if (string.Compare(games[inner,0], games[inner + 1, 0]) > 0)
+                    {
+                        // saving inner + 1 array into a temp string
+                        // replacing inner + 1 with inner
+                        // replacing inner with tmp, which is actually inner + 1
+                        string tmp = games[inner + 1, 0];
+                        games[inner + 1, 0] = games[inner, 0];
+                        games[inner, 0] = tmp;
+
+                        tmp = games[inner + 1, 1];
+                        games[inner + 1, 1] = games[inner, 1];
+                        games[inner, 1] = tmp;
+
+                        tmp = games[inner + 1, 2];
+                        games[inner + 1, 2] = games[inner, 2];
+                        games[inner, 2] = tmp;
+
+                        tmp = games[inner + 1, 3];
+                        games[inner + 1, 3] = games[inner, 3];
+                        games[inner, 3] = tmp;
+                    }
+                }
+            }
+        }
+
+        // binary search method
+        private int BinarySearch(string[,] games, string search)
+        {
+            int lowerBound = 0;
+            int upperBound = numItems;
+            int mid;
+
+            while (true)
+            {
+                // find middle by plusing lower and upper and dividing by 2
+                mid = (lowerBound + upperBound) / 2;
+
+                // if games[mid, 0] is the searched item then return it
+                if (games[mid, 0] == search)
+                {
+                    return mid;
+                }
+                else if (lowerBound > upperBound)
+                {
+                    return -1;
+                }
+                else
+                {
+                    // if the compared string is bigger then 0 decrease mid by 1
+                    if (string.Compare(games[mid, 0], search, false) > 0)
+                    {
+                        upperBound = mid - 1;
+                    }
+                    // else increase mid by 1
+                    else
+                    {
+                        lowerBound = mid + 1;
+                    }
+                }
+            }
+        }
+
+        // method to clear all textboxes
+        public void Clear()
+        {
+            tbGameTitle.Clear();
+            tbPlatform.Clear();
+            tbGenre.Clear();
+            tbPublisher.Clear();
+        }
+
+        // method to display games to lstOutput
+        public void DisplayGames()
+        {
+            // clear all items in lstOutput
+            lstOutput.Items.Clear();
+
+            // call BubbleSort method with games array
+            BubbleSort(games);
+
+            // display games array in lstOutput
+            for (int i = 0; i < numItems; i++)
+            {
+                lstOutput.Items.Add(games[i, 0] + ", " + games[i, 1] + ", " + games[i, 2] + ", " + games[i, 3]);
+            }
+        }
+    }
+}
