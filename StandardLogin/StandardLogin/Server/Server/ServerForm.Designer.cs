1eec8b549b938ead09e3f3dec6dfd70273d01b9a
LukeGough <37359964+LukeGough@users.noreply.github.com>
2019-10-16 12:18:23 +0800

    Add files via upload

---
+++ StandardLogin/StandardLogin/Server/Server/ServerForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Server
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbPipeName = new System.Windows.Forms.TextBox();
+            this.lbPipeName = new System.Windows.Forms.Label();
+            this.tbReceived = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(237, 25);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 20);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Server";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbPipeName
+            // 
+            this.tbPipeName.Location = new System.Drawing.Point(12, 25);
+            this.tbPipeName.Name = "tbPipeName";
+            this.tbPipeName.ReadOnly = true;
+            this.tbPipeName.Size = new System.Drawing.Size(219, 20);
+            this.tbPipeName.TabIndex = 1;
+            this.tbPipeName.Text = "\\\\.\\pipe\\myNamedPipe";
+            // 
+            // lbPipeName
+            // 
+            this.lbPipeName.AutoSize = true;
+            this.lbPipeName.Location = new System.Drawing.Point(12, 9);
+            this.lbPipeName.Name = "lbPipeName";
+            this.lbPipeName.Size = new System.Drawing.Size(62, 13);
+            this.lbPipeName.TabIndex = 0;
+            this.lbPipeName.Text = "Pipe Name:";
+            // 
+            // tbReceived
+            // 
+            this.tbReceived.Location = new System.Drawing.Point(12, 51);
+            this.tbReceived.Multiline = true;
+            this.tbReceived.Name = "tbReceived";
+            this.tbReceived.ReadOnly = true;
+            this.tbReceived.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbReceived.Size = new System.Drawing.Size(300, 269);
+            this.tbReceived.TabIndex = 3;
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 331);
+            this.Controls.Add(this.tbReceived);
+            this.Controls.Add(this.lbPipeName);
+            this.Controls.Add(this.tbPipeName);
+            this.Controls.Add(this.btnStart);
+            this.Name = "ServerForm";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbPipeName;
+        private System.Windows.Forms.Label lbPipeName;
+        private System.Windows.Forms.TextBox tbReceived;
+    }
+}
+
---
+++ StandardLogin/StandardLogin/Server/Server/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class UserRepository
+    {
+        List<User> users = new List<User>();
+
+        // function to add the user in memory dummy db
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+
+        // function to retrieve the user based on the user id
+        public User GetUser(string userid)
+        {
+            return users.Single(u => u.UserId == userid);
+        }
+
+        // function to check if a User's UserId
+        public bool UserExists(string userid)
+        {
+            try
+            {
+                // (users.Single(u => u.UserId == userid)
+                if (users.Any(cus => cus.UserId == userid))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                } 
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
